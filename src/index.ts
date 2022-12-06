a0ccde1722ca4d9bacbf367f6fd5fda3a324e9c7
Thiago <thdev@gmail.com>
2022-12-06 11:55:29 +0000

    separating server.ts from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
 import dotenv from "dotenv"
-
 dotenv.config();
 const PORT = process.env.PORT
-
-import express from "express"
-import bodyParser from "body-parser"
-import { useRoutes } from "./routes/index"
-
-const app = express()
-app.use(bodyParser.json())
-useRoutes(app)
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.json({ msg: "ok" })
-
-})
+import {app} from "./server/server"
 
 app.listen(PORT, () =>
     console.log("Listening at " + PORT))
+
+    
---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import bodyParser from "body-parser"
+import { useRoutes } from "../routes/index"
+
+const app = express()
+app.use(bodyParser.json())
+useRoutes(app)
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.json({ msg: "ok" })
+})
+
+export {app}
